e9f1441bca5e8e251db6a7ea4ccd2d9c78d1ef3a
Joe Lewis <joelewis101@doctors.org.uk>
2022-07-11 09:52:47 +0100

    add lstmed to bogofilter, misc mailcap adns

--- neomutt/.config/neomutt/bogofilter_spam.sh
+++ neomutt/.config/neomutt/bogofilter_spam.sh
@@ -70,3 +70,39 @@ then
 else
         echo "No new mails to check"
 fi
+
+
+echo "Checking lstmed"
+SRC=~/Mail/lstmed/INBOX/new
+DST=~/Mail/lstmed/Junk/new
+
+if [ -n "$(ls $SRC)" ]
+then
+        echo "Mail tagged as new"
+        echo "Checking..."
+        #FILES=$(rg -l -F "Received" $SRC)
+        COUNT=0
+        FILES=($SRC/*)
+        #echo $FILES
+        for file in $FILES; do
+                RESULT=$(/opt/homebrew/bin/bogofilter -o0.5 -I $file -v) 
+            #echo $file
+            #echo $RESULT
+            filename=$(basename $file)
+            #echo $filename
+            if [[ $RESULT == *Spam* ]]; then
+                    if ! grep -q "\[Bogon\]" $file; then
+                       /opt/homebrew/bin/bogofilter -o0.5 -I $file -p --spam-subject-tag "[Bogon]" > ~/tmp/$filename
+                       rm $file
+                       mv ~/tmp/$filename $file
+                       #echo "SPAM!!!"
+                       COUNT=$((COUNT+1))
+                    fi
+            fi
+        done
+
+        echo $COUNT "new bogons identfied"
+
+else
+        echo "No new mails to check"
+fi
