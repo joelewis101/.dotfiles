f4393dfdf8cdcb5cfd7e51edb29b512304605211
Joe Lewis <joelewis101@doctors.org.uk>
2023-01-04 16:06:45 +0000

    Add to neomutt config - inc imapfilter workflow

--- neomutt/.config/neomutt/sync_mail.sh
+++ neomutt/.config/neomutt/sync_mail.sh
@@ -1,12 +1,15 @@
 #!/bin/zsh
 echo "Running joe's mail sync script"
 echo  $(date)
+echo "Running imapfilter/bogofilter"
+killall imapfilter &> /dev/null
+/opt/homebrew/bin/imapfilter
 echo "Running mbsync"
 killall mbsync &>/dev/null
 /opt/homebrew/bin/mbsync -a 
 echo "Running notmuch"
 killall notmuch &>/dev/null; 
 /opt/homebrew/bin/notmuch new 
-echo "Running bogofilter"
-/Users/joelewis/.config/neomutt/bogofilter_spam.sh
+# echo "Running bogofilter"
+# /Users/joelewis/.config/neomutt/bogofilter_spam.sh
 echo "Done. Share and Enjoy!"
