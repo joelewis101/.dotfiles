e8b7d22f841c3024b3913c290275d864fa724f48
Joe Lewis <joelewis101@doctors.org.uk>
2024-07-02 16:05:57 +0100

    Script changes for new macbook

--- khal/.config/khal/sync_cal.sh
+++ khal/.config/khal/sync_cal.sh
@@ -3,5 +3,9 @@ echo "Running joe's calendar sync script"
 echo $(date)
 echo "Running vdirsyncer"
 killall vdirsyncer &>/dev/null
+if [ ! -f ~/.config/vdirsyncer/token_file ]; then
+    echo "No google token file"
+    vdirsyncer discover
+fi
 /opt/homebrew/bin/vdirsyncer sync
 echo "Done. Share and Enjoy!"
